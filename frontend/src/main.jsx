cf058ff3e056e861dfa1dd50dc8fe73c684a4f78
bhargavasai <cherukuribhargavsai@gmail.com>
2026-06-30 11:11:34 +0530

    update cors for production

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<AuthContextProvider>
 				<SocketContextProvider>
 					<App />
 				</SocketContextProvider>
 			</AuthContextProvider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
