192c1340b09d839b20fc8cdc756bc659552e4390
bhargavasai <cherukuribhargavsai@gmail.com>
2026-07-03 16:09:50 +0530

    logout fix

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,25 +1,37 @@
 import { useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 const useLogout = () => {
 	const [loading, setLoading] = useState(false);
 	const { setAuthUser } = useAuthContext();
+	const navigate = useNavigate();
 
 	const logout = async () => {
 		setLoading(true);
+
 		try {
-			const res = await fetch("/api/auth/logout", {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-			});
+			const res = await fetch(
+				"https://real-time-chat-application-52rd.onrender.com/api/auth/logout",
+				{
+					method: "POST",
+					credentials: "include",
+				}
+			);
+
 			const data = await res.json();
-			if (data.error) {
-				throw new Error(data.error);
+
+			if (!res.ok) {
+				throw new Error(data.error || data.message);
 			}
 
 			localStorage.removeItem("chat-user");
 			setAuthUser(null);
+
+			toast.success("Logged out");
+
+			navigate("/login");
 		} catch (error) {
 			toast.error(error.message);
 		} finally {
@@ -29,4 +41,5 @@ const useLogout = () => {
 
 	return { loading, logout };
 };
+
 export default useLogout;
